c0939d52743aaf1c81273be92db18914a937fdb8
nagadeepakbhushan <nagadeepak000@gmail.com>
2021-03-09 13:36:30 +0530

    navbar-added-styles-required

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-    
+    <navBar/>
   <h1>hello</h1>
   <helloWorld/>
   </div>
 </template>
 <script>
+import navbar from './components/navbar'
 export default {
- 
+ components: {
+   navBar: navbar
+ }
 }
 </script>
 <style lang="scss">
-
+*{
+  box-sizing: border-box;
+  margin:0%;
+}
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="navbar">
+      <div class="logo">
+        <span ><img src="../../public/Quinbook.png" alt="logo" srcset="" width="40px" height="40px" id="quinlogo"></span>
+      </div>
+      <div class="searchbar">
+          <input type="text" placeholder="search friends">
+          <button>search</button>
+      </div>
+      <div class="notifications">
+          <div class="notifications">
+              <button>notifications</button>
+              <button>profile</button>
+          </div> 
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#navbar {
+    background-color:#3AED91  ;
+    height: 60px;
+    width: 100%;
+    display: flex;
+    align-content: space-between;
+    flex-wrap: wrap;
+    /* justify-content: flex-start; */
+}
+.logo {
+    padding-top: 8px;
+    margin-left: 30px; 
+    display: inline-block;
+}
+#quinlogo{
+    box-shadow: 1px 1px 1px 1px black;
+}
+.searchbar{
+     padding-left: 20%;
+}
+.notifications{
+     
+}
+
+
+    
+</style>
